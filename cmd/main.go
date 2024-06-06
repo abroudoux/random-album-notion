1639725dadd89ea49ef136d25efdd4e9cf0e7109
arthur <arthur.broudoux@gmail.com>
2024-06-06 23:39:11 +0200

    feature: load env variables and create connection to Notion API

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/jomei/notionapi"
 )
 
 func main () {
-	fmt.Println("Hello world !")
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file", err)
+	}
+
+	envApiKey := "NOTION_API_KEY"
+	envPageId := "NOTION_PAGE_ID"
+	notionApiKey := os.Getenv(envApiKey)
+	notionPageId := os.Getenv(envPageId)
+
+	client := notionapi.NewClient(notionapi.Token(notionApiKey))
+
+	fmt.Println(notionApiKey, notionPageId, client)
+
+	page, err := client.Page.Get(context.Background(), notionapi.PageID(notionPageId))
+
+	if err != nil {
+		fmt.Println("Error connecting page", err)
+	}
+
+	fmt.Println(page)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/abroudoux/random-album
 
 go 1.21.5
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jomei/notionapi v1.13.0 // indirect
+)
