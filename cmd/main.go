4990e3653fde8e0f6acb506e044282a185330dc6
arthur <arthur.broudoux@gmail.com>
2024-06-07 11:16:50 +0200

    feature: get page content (todo blocks)

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/jomei/notionapi"
 )
 
-func main () {
+const notionAPIVersion string = "2022-06-28"
+
+func Load() (string, string) {
 	err := godotenv.Load()
 
 	if err != nil {
@@ -21,15 +24,49 @@ func main () {
 	notionApiKey := os.Getenv(envApiKey)
 	notionPageId := os.Getenv(envPageId)
 
-	client := notionapi.NewClient(notionapi.Token(notionApiKey))
+	return notionApiKey, notionPageId
+}
 
-	fmt.Println(notionApiKey, notionPageId, client)
+func GetPage(notionPageId string, notionApiKey string, notionAPIVersion string) (string, error) {
+	url := "https://api.notion.com/v1/blocks/" + notionPageId + "/children"
 
-	page, err := client.Page.Get(context.Background(), notionapi.PageID(notionPageId))
+	var bearer = "Bearer " + notionApiKey
+
+	req, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
-		fmt.Println("Error connecting page", err)
+		log.Fatalln(err)
 	}
 
-	fmt.Println(page)
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Notion-Version", notionAPIVersion)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+    if err != nil {
+        log.Println("Error while reading the response bytes:", err)
+    }
+
+	return string([]byte(body)), nil
+}
+
+func main () {
+	notionApiKey, notionPageId := Load()
+
+	res, err := GetPage(notionPageId, notionApiKey, notionAPIVersion)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res)
 }
