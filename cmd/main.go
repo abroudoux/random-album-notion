a6c8f75affbd5775535799dd0661f4b21a9ba676
arthur <arthur.broudoux@gmail.com>
2024-06-07 14:06:27 +0200

    feature: select random album

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 
@@ -11,10 +13,27 @@ import (
 )
 
 const notionAPIVersion string = "2022-06-28"
+type Block struct {
+	Object string `json:"object"`
+	ID     string `json:"id"`
+	Type   string `json:"type"`
+	ToDo   *struct {
+		RichText []struct {
+			Text struct {
+				Content string `json:"content"`
+			} `json:"text"`
+		} `json:"rich_text"`
+		Checked bool `json:"checked"`
+	} `json:"to_do,omitempty"`
+}
+
+type NotionResponse struct {
+	Object  string  `json:"object"`
+	Results []Block `json:"results"`
+}
 
 func Load() (string, string) {
 	err := godotenv.Load()
-
 	if err != nil {
 		fmt.Println("Error loading .env file", err)
 	}
@@ -27,15 +46,14 @@ func Load() (string, string) {
 	return notionApiKey, notionPageId
 }
 
-func GetPage(notionPageId string, notionApiKey string, notionAPIVersion string) (string, error) {
+func GetPage(notionPageId string, notionApiKey string, notionAPIVersion string) ([]string, error) {
 	url := "https://api.notion.com/v1/blocks/" + notionPageId + "/children"
-
 	var bearer = "Bearer " + notionApiKey
 
 	req, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("error creating request: %v", err)
 	}
 
 	req.Header.Add("Authorization", bearer)
@@ -45,28 +63,54 @@ func GetPage(notionPageId string, notionApiKey string, notionAPIVersion string) 
 	resp, err := client.Do(req)
 
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("error making request: %v", err)
 	}
 
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 
-    if err != nil {
-        log.Println("Error while reading the response bytes:", err)
-    }
-
-	return string([]byte(body)), nil
-}
-
-func main () {
-	notionApiKey, notionPageId := Load()
-
-	res, err := GetPage(notionPageId, notionApiKey, notionAPIVersion)
-
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
-	fmt.Println(res)
+	var notionResponse NotionResponse
+	err = json.Unmarshal(body, &notionResponse)
+
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON response: %v", err)
+	}
+
+	var todos []string
+	
+	for _, block := range notionResponse.Results {
+		if block.Type == "to_do" && block.ToDo != nil && !block.ToDo.Checked {
+			for _, richText := range block.ToDo.RichText {
+				todos = append(todos, richText.Text.Content)
+			}
+		}
+	}
+
+	return todos, nil
+}
+
+func ChooseRandomAlbum(albums []string) string {
+	randomNb := rand.Intn(len(albums))
+	randomAlbum := albums[randomNb]
+
+	return randomAlbum
+}
+
+func main() {
+	notionApiKey, notionPageId := Load()
+
+	todos, err := GetPage(notionPageId, notionApiKey, notionAPIVersion)
+
+	if err != nil {
+		log.Fatalf("Error fetching page: %v", err)
+	}
+
+	randomAlbum := ChooseRandomAlbum(todos)
+
+	fmt.Println(randomAlbum)
 }
